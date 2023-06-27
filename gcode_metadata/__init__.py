5542a8f08a174c11ff2f7792690dd37b7e7e2bd1
Michal Zoubek <michal.zoubek@prusa3d.cz>
2023-06-27 14:46:56 +0200

    Added function for getting biggest thumbnail from list of thumbnails

--- gcode_metadata/__init__.py
+++ gcode_metadata/__init__.py
@@ -1,4 +1,4 @@
 """Init file describing every importable object"""
 
 from .metadata import MetaData, FDMMetaData, SLMetaData, get_metadata, \
-    UnknownGcodeFileType,estimated_to_seconds
+    UnknownGcodeFileType, estimated_to_seconds, biggest_resolution
