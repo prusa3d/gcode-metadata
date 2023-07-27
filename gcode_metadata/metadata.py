c0cb61a49e0237b0c41fa61f6e1273385b05d237
Michal Zoubek <michal.zoubek@prusa3d.cz>
2023-07-27 09:33:36 +0200

    Updated obtaining thumbnail and its format

--- gcode_metadata/metadata.py
+++ gcode_metadata/metadata.py
@@ -324,6 +324,7 @@ class FDMMetaData(MetaData):
         "normal_change_in_present": bool,
         "layer_info_present": bool,
         "max_layer_z": float,
+        "thumbnails_format": str,
     }
 
     # Add attributes that have multiple values in MMU print gcodes
