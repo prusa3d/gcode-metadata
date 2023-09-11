1347350d229b5433bb317110366dd3393884e1d9
Ondrej Tuma <mcbig@zeropage.cz>
2023-09-11 13:42:55 +0200

    Public release 0.1.0

--- gcode_metadata/__init__.py
+++ gcode_metadata/__init__.py
@@ -4,6 +4,17 @@ from .metadata import MetaData, FDMMetaData, SLMetaData, get_metadata, \
     UnknownGcodeFileType, estimated_to_seconds, biggest_resolution, \
     get_meta_class
 
+__version__ = "0.1.0"
+__date__ = "11 Sep 2023"  # version date
+__copyright__ = "(c) 2023 Prusa 3D"
+__author_name__ = "Prusa Connect Developers"
+__author_email__ = "link@prusa3d.cz"
+__author__ = f"{__author_name__} <{__author_email__}>"
+__description__ = "Python library for extraction of metadata from g-code files"
+
+__credits__ = "Ondřej Tůma, Michal Zoubek, Tomáš Jozífek, Šárka Faloutová"
+__url__ = "https://github.com/prusa3d/gcode-metadata"
+
 __all__ = [
     "MetaData", "FDMMetaData", "SLMetaData", "get_metadata",
     "UnknownGcodeFileType", "estimated_to_seconds", "biggest_resolution",
