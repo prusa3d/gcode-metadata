c0048e3cfaf9b3285251193f0e51f9703e41c633
sarka.faloutova <sarka.faloutova@outlook.cz>
2024-07-01 09:41:33 +0200

    fix mmu attributes parsing

--- gcode_metadata/metadata.py
+++ gcode_metadata/metadata.py
@@ -23,18 +23,18 @@ RE_ESTIMATED = re.compile(r"((?P<days>[0-9]+)d\s*)?"
                           r"((?P<seconds>[0-9]+)s)?")
 
 PRINTERS = [
-        'MK4IS', 'MK4MMU3', 'MK4', 'MK3SMMU3', 'MK3MMU3', 'MK3SMMU2S',
-        'MK3MMU2', 'MK3S', 'MK3', 'MK2.5SMMU2S', 'MK2.5MMU2', 'MK2.5S',
-        'MK2.5', 'MK3.5', 'MK3.9', 'MINI', 'MINIIS', 'XL5', 'XL4', 'XL3',
-        'XL2', 'XL', 'iX', 'SL1', 'SHELF', 'EXTRACTOR', 'HARVESTER'
-    ]
+    'MK4IS', 'MK4MMU3', 'MK4', 'MK3SMMU3', 'MK3MMU3', 'MK3SMMU2S', 'MK3MMU2',
+    'MK3S', 'MK3', 'MK2.5SMMU2S', 'MK2.5MMU2', 'MK2.5S', 'MK2.5', 'MK3.5',
+    'MK3.9', 'MINI', 'MINIIS', 'XL5', 'XL4', 'XL3', 'XL2', 'XL', 'iX', 'SL1',
+    'SHELF', 'EXTRACTOR', 'HARVESTER'
+]
 
 PRINTERS.sort(key=len, reverse=True)
 
 MATERIALS = [
-    'PLA', 'PETG', 'ABS', 'ASA', 'FLEX', 'HIPS', 'EDGE', 'NGEN', 'PA',
-    'PVA', 'PCTG', 'PP', 'PC', 'PEBA', 'CPE', 'PVB', 'PET', 'PLA Tough',
-    'METAL', 'TPU', 'NYLON'
+    'PLA', 'PETG', 'ABS', 'ASA', 'FLEX', 'HIPS', 'EDGE', 'NGEN', 'PA', 'PVA',
+    'PCTG', 'PP', 'PC', 'PEBA', 'CPE', 'PVB', 'PET', 'PLA Tough', 'METAL',
+    'TPU', 'NYLON'
 ]
 
 IMAGE_FORMATS = ['PNG', 'JPG']
@@ -440,6 +440,7 @@ class MMUAttribute:
 
 class FDMMetaData(MetaData):
     """Class for extracting Metadata for FDM gcodes"""
+
     # pylint: disable=too-many-instance-attributes
 
     def set_attr(self, name, value):
@@ -613,8 +614,7 @@ class FDMMetaData(MetaData):
 
         # We store the image data only during parsing. If actively parsing:
         if self.img is not None:
-            line = line[2:].strip()
-            self.img.append(line)
+            self.img.append(line[2:].strip())
 
         # For the bulk of metadata comments
         match = self.KEY_VAL_PAT.match(line)
