78fc2d83f23a5b456bb40566e5f9817737e1de80
Tomáš Jozífek <jozifektomas@gmail.com>
2025-02-14 12:13:14 +0100

    Add m1 file extension to sla extensions

--- gcode_metadata/__init__.py
+++ gcode_metadata/__init__.py
@@ -4,9 +4,9 @@ from .metadata import MetaData, FDMMetaData, SLMetaData, get_metadata, \
     UnknownGcodeFileType, estimated_to_seconds, get_preview, get_icon, \
     get_meta_class, PRINTERS
 
-__version__ = "0.2.0"
-__date__ = "5 May 2024"  # version date
-__copyright__ = "(c) 2023 Prusa 3D"
+__version__ = "0.2.1"
+__date__ = "14 Feb 2025"  # version date
+__copyright__ = "(c) 2025 Prusa 3D"
 __author_name__ = "Prusa Connect Developers"
 __author_email__ = "link@prusa3d.cz"
 __author__ = f"{__author_name__} <{__author_email__}>"
