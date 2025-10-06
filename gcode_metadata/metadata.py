914698a73f9fc8482e53f1e7ca501e266d7b75de
Šárka Faloutová <65911928+sarkafa@users.noreply.github.com>
2025-10-06 09:35:23 +0200

    new slx extensions (#58)

--- gcode_metadata/metadata.py
+++ gcode_metadata/metadata.py
@@ -14,7 +14,7 @@ from importlib.metadata import version
 # pylint: disable=too-many-lines
 
 GCODE_EXTENSIONS = (".gcode", ".gc", ".g", ".gco")
-SLA_EXTENSIONS = ("sl1", "sl1s", "m1")
+SLA_EXTENSIONS = ("sl1", "sl1s", "m1", "sl2", "slx")
 CHARS_TO_REMOVE = ["/", "\\", "\"", "(", ")", "[", "]", "'"]
 
 log = getLogger("connect-printer")
